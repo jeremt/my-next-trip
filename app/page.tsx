a8ac7730cd52ae1bd46d08ce5034e4910bfab48e
jeremt <taboada.jeremie@gmail.com>
2026-03-17 12:00:41 +0100

    feat: add comment count to destis

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,26 @@
 import {db} from "@/lib/db/drizzle";
-import {destinations} from "@/lib/db/schema";
+import {comments, destinations} from "@/lib/db/schema";
+import {eq, sql} from "drizzle-orm";
 
 export default async function Home() {
-    const destis = await db.select().from(destinations);
+    const destis = await db
+        .select({
+            id: destinations.id,
+            name: destinations.name,
+            description: destinations.description,
+            commentCount: sql<number>`count(${comments.id})`.mapWith(Number),
+        })
+        .from(destinations)
+        .leftJoin(comments, eq(destinations.id, comments.destinationId))
+        .groupBy(destinations.id);
     return (
         <div>
             <h1 className="text-2xl">🌴 My next trip</h1>
             {destis.map((desti) => (
                 <div key={desti.id}>
-                    <h2 className="text-lg">{desti.name}</h2>
+                    <h2 className="text-lg">
+                        {desti.name} ({desti.commentCount})
+                    </h2>
                     <p>{desti.description}</p>
                 </div>
             ))}
